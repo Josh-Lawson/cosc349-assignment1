95285d377b0e40fd6710226462fd7546d6d0ed55
josh-10110 <joshlawson99@outlook.com>
2023-08-29 23:19:35 +1200

    basic php files for admin and user interfaces

---
+++ www/admin/admin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+
+<head>
+    <title>Admin Interface</title>
+    <style>
+        th {
+            text-align: left;
+        }
+
+        table,
+        th,
+        td {
+            border: 2px solid grey;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 0.2em;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Recipe Management</h1>
+    <p><a href="add_recipe.php">Add Recipe</a></p>
+    <p><a href="edit_recipe.php">Edit Recipe</a></p>
+    <p><a href="delete_recipe.php">Delete Recipe</a></p>
+
+    <h1>User Management</h1>
+    <p><a href="add_user.php">Add User</a></p>
+    <p><a href="edit_user.php">Edit User</a></p>
+    <p><a href="delete_user.php">Delete User</a></p>
+</body>
+
+</html>
---
+++ www/user/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+
+<head>
+    <title>Recipe Management System</title>
+    <style>
+        th {
+            text-align: left;
+        }
+
+        table,
+        th,
+        td {
+            border: 2px solid grey;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            padding: 0.2em;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Recipe Management</h1>
+    <p><a href="add_recipe.php">Add Recipe</a></p>
+    <p><a href="edit_recipe.php">Edit Recipe</a></p>
+    <p><a href="delete_recipe.php">Delete Recipe</a></p>
+
+    
+
+
+</body>
+
+</html>
