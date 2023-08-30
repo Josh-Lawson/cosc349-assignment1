dff22e37e87305fd4b1bb00b3ceebc7feb85833a
josh-10110 <joshlawson99@outlook.com>
2023-08-30 18:12:50 +1200

    updated navbar to include a return to home button

--- www/common/navbar.php
+++ www/common/navbar.php
@@ -4,6 +4,26 @@ if (!isset($_SESSION['userId'])) {
     header('Location: ../common/sign_in.php');
     exit();
 }
+
+$servername = "192.168.56.12";
+$dbusername = "admin";
+$dbpassword = "admin_pw";
+$dbname = "RecipeManagementSystem";
+
+$conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$userId = $_SESSION['userId'];
+$stmt = $conn->prepare("SELECT * FROM Admin WHERE adminId = ?");
+$stmt->bind_param("i", $userId);
+$stmt->execute();
+$isAdmin = $stmt->fetch();
+$stmt->close();
+$conn->close();
+
 ?>
 
 <!DOCTYPE html>
@@ -22,6 +42,8 @@ if (!isset($_SESSION['userId'])) {
             if (isset($_SESSION['username'])) {
                 echo 'Welcome, ' . $_SESSION['username'];
                 ?>
+                <form action="<?php echo $isAdmin ? 'http://127.0.0.1:8081/admin.php' : 'http://127.0.0.1:8080/'; ?>" method="GET" style="display:inline;">
+                    <button type="submit">Home</button>
                 <form action="../common/sign_out.php" method="POST" style="display:inline;">
                     <button type="submit">Sign Out</button>
                 </form>
