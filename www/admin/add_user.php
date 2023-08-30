7a6955722af42375005cb9a90e2a0b8adfaf7772
josh-10110 <joshlawson99@outlook.com>
2023-08-30 16:04:27 +1200

    update to add user function

--- www/admin/add_user.php
+++ www/admin/add_user.php
@@ -10,16 +10,17 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+$name = $_POST["name"];
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$stmt = $conn->prepare("INSERT INTO User (username, password) VALUES (?, ?)");
+$stmt = $conn->prepare("INSERT INTO User (name, username, password) VALUES (?, ?, ?)");
 
 if (!$stmt) {
     echo "Prepare failed: (" . $conn->errno . ") " . $conn->error;
 }
 
-$stmt->bind_param("ss", $username, $password);
+$stmt->bind_param("sss", $name, $username, $password);
 
 $stmt->execute();
 
@@ -32,30 +33,23 @@ $conn->close();
 
 <html>
     <head>
+    <header>
+        <?php include '../common/navbar.php'; ?>
+</header>
         <title>Add User</title>
-        <style>
-            th {
-                text-align: left;
-            }
-
-            table, th, td {
-                border: 2px solid grey;
-                border-collapse: collapse;
-            }
-
-            th, td {
-                padding: 0.2em;
-            }
-        </style>
+        <link rel="stylesheet" type="text/css" href="../common/style/style.css">
     </head>
     <body>
+        <main>
         <h1>Add User</h1>
         <p><a href="admin.php">Admin Interface</a></p>
 
         <form action = "add_user.php" method = "POST">
+            <p>name: <input type = "text" name = "name" /></p>
             <p>Username: <input type = "text" name = "username" /></p>
             <p>Password: <input type = "text" name = "password" /></p>
             <p><input type = "submit" value = "Add User" /></p>
         </form>
+        </main
     </body>
 </html>
