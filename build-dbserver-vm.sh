a3e09324ce062d549ae8165dfed705f3ae6fca83
josh-10110 <joshlawson99@outlook.com>
2023-08-31 23:11:27 +1200

    csv data loaded into tables using shell script #2

--- build-dbserver-vm.sh
+++ build-dbserver-vm.sh
@@ -10,10 +10,17 @@ apt-get -y install mysql-server mysql-client
 
 service mysql start
 
+echo "DROP DATABASE IF EXISTS RecipeManagementSystem;" | mysql
 echo "CREATE DATABASE RecipeManagementSystem;" | mysql
+
+echo "DROP USER IF EXISTS 'admin'@'%';" | mysql
 echo "CREATE USER 'admin'@'%' IDENTIFIED BY 'admin_pw';" | mysql
+
 echo "GRANT ALL PRIVILEGES ON RecipeManagementSystem.* TO 'admin'@'%'" | mysql
+
+echo "DROP USER IF EXISTS 'user'@'%';" | mysql
 echo "CREATE USER 'user'@'%' IDENTIFIED BY 'user_pw';" | mysql
+
 echo "GRANT SELECT, INSERT, UPDATE, DELETE ON RecipeManagementSystem.* TO 'user'@'%'" | mysql
 echo FLUSH PRIVILEGES | mysql
 
@@ -24,4 +31,14 @@ cat /vagrant/setup-database.sql | mysql -u admin -p$MYSQL_ADMIN_PWD RecipeManage
 
 sed -i'' -e '/bind-address/s/127.0.0.1/0.0.0.0/' /etc/mysql/mysql.conf.d/mysqld.cnf
 
+mysql -u root -p$MYSQL_PWD -e "SHOW VARIABLES LIKE 'secure_file_priv';"
+
 service mysql restart
+
+rm /var/lib/mysql-files/*
+cp /vagrant/data/*.csv /var/lib/mysql-files/
+ls /var/lib/mysql-files/ 
+
+mysql -u root -p$MYSQL_PWD RecipeManagementSystem -e "LOAD DATA INFILE '/var/lib/mysql-files/ingredients.csv' INTO TABLE Ingredient FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;"
+
+cat /vagrant/insert-data.sql | mysql -u admin -p$MYSQL_ADMIN_PWD RecipeManagementSystem
--- setup-database.sql
+++ setup-database.sql
@@ -16,15 +16,16 @@ create table if not exists Admin (
 
 create table if not exists Ingredient (
     ingredientId int primary key auto_increment,
-    ingredientName varchar(255) not null
+    ingredientName varchar(512) not null
 );
 
 create table if not exists Recipe (
-    recipeId int primary key auto_increment,
+    recipeId int primary key auto_increment UNIQUE,
     userId int,
     recipeName varchar(255) not null,
-    approved boolean default false,
-    instructions text
+    instructions text,
+    approved boolean default false
+    
 );
 
 create table if not exists RecipeIngredient (
@@ -35,55 +36,3 @@ create table if not exists RecipeIngredient (
     foreign key (recipeId) references Recipe(recipeId),
     foreign key (ingredientId) references Ingredient(ingredientId)
 );
-
--- dummy data:
-
-insert into User (name, username, password) values ('john', 'user1', 'password1');
-insert into User (name, username, password) values ('bob', 'user2', 'password2');
-
-insert into Admin (username, password) values ('admin1', 'password1');
-insert into Admin (username, password) values ('admin2', 'password2');
-
-insert into Ingredient (ingredientName) values ('ingredient1');
-insert into Ingredient (ingredientName) values ('ingredient2');
-insert into Ingredient (ingredientName) values ('ingredient3');
-
-insert into Recipe (userId, recipeName, description) values (1, 'recipe1', 'description1');
-insert into Recipe (userId, recipeName, description) values (1, 'recipe2', 'description2');
-insert into Recipe (userId, recipeName, description) values (2, 'recipe3', 'description3');
-
-insert into RecipeIngredient (recipeId, ingredientId, quantity) values (1, 1, '1 cup');
-insert into RecipeIngredient (recipeId, ingredientId, quantity) values (1, 2, '2 cups');
-insert into RecipeIngredient (recipeId, ingredientId, quantity) values (2, 1, '3 cups');
-insert into RecipeIngredient (recipeId, ingredientId, quantity) values (2, 2, '4 cups');
-insert into RecipeIngredient (recipeId, ingredientId, quantity) values (3, 1, '5 cups');
-
-
--- extra tables for extra features:
-
--- create table if not exists Review (
---     reviewId int primary key auto_increment,
---     userId int,
---     recipeId int,
---     rating int,
---     reviewText text,
---     approved boolean default false,
---     foreign key (userId) references User(userId),
---     foreign key (recipeId) references Recipe(recipeId)
--- );
-
--- create table if not exists Image (
---     recipeId int not null,
---     imageNumber int not null,
---     imageURL varchar(255) not null,
---     approved boolean default false,
---     primary key (recipeId, imageNumber),
---     foreign key (recipeId) references Recipe(recipeId)
--- );
-
--- create table Feedback (
---     feedbackId int primary key auto_increment,
---     userId int,
---     feedback text,
---     foreign key (userId) references User(userId)
--- );
